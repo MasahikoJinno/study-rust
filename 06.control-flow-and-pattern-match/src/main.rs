756ff069ee6a44abaf3bd9d90722717ac867ed4b
Masahiko Jinno <mjinno@mbp14.local>
2022-02-18 00:46:25 +0900

    fix: 06

--- 06.control-flow-and-pattern-match/src/main.rs
+++ 06.control-flow-and-pattern-match/src/main.rs
@@ -30,6 +30,65 @@ fn main() {
     };
 
     println!("x={}", x);
+
+    // ベクターの数分繰り返し処理を行う
+    let v = vec![10, 20, 30, 40, 50];
+    print!("v=[");
+    for i in &v {
+        let x: i32 = *i;
+        print!("{}, ", x);
+    }
+    println!("]");
+
+    // イテレータを使用して繰り返し処理をする
+    let v = vec![10, 20, 30, 40, 50];
+    print!("v=[");
+    for i in v.iter() {
+        let x: i32 = *i;
+        print!("{}, ", x);
+    }
+    println!("]");
+
+    // イテレータを使用して、インデックス付きの繰り返し処理をする
+    let v = vec![10, 20, 30, 40, 50];
+    print!("v=[");
+    for (i, x) in v.iter().enumerate() {
+        print!("{}:{}, ", i, x);
+    }
+    println!("]");
+
+    // 指定回数繰り返す
+    for i in 0..10 {
+        print!("{} ", i);
+    }
+    println!("");
+
+    // 負の数でも良いっぽい
+    for i in -5..-1 {
+        print!("{} ", i);
+    }
+    println!("");
+
+    // while
+    print!("while = ");
+    let mut i = 0;
+    while i < 10 {
+        print!("{} ", i);
+        i += 1;
+    }
+    println!("");
+
+    // loop
+    print!("loop = ");
+    let mut i = 0;
+    loop {
+        if i >= 10 {
+            break;
+        }
+        print!("{} ", i);
+        i += 1;
+    }
+    println!("");
 }
 
 fn equal(a: i32, b: i32) -> bool {
